d708b1cd7eeaacae5638ef77d64a5c265656e715
blde99 <matt.g.parkinson@gmail.com>
2018-01-08 10:16:58 +0000

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,20 @@
 #include <Arduino.h>
+#include "Bounce2.h" // for button debouncing
+#include <Wire.h>  // i2c library
 
-/**
- * The MIT License (MIT)
- *
- * Copyright (c) 2016 by Daniel Eichhorn
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- */
-
-// Include the correct display library
-// For a connection via I2C using Wire include
-#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
-#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
-// or #include "SH1106.h" alis for `#include "SH1106Wire.h"`
-// For a connection via I2C using brzo_i2c (must be installed) include
-// #include <brzo_i2c.h> // Only needed for Arduino 1.6.5 and earlier
-// #include "SSD1306Brzo.h"
-// #include "SH1106Brzo.h"
-// For a connection via SPI include
-// #include <SPI.h> // Only needed for Arduino 1.6.5 and earlier
-// #include "SSD1306Spi.h"
-// #include "SH1106SPi.h"
-
-// Include custom images
+// Includes for OLED screen
 #include "images.h"
-
-// Initialize the OLED display using SPI
-// D5 -> CLK
-// D7 -> MOSI (DOUT)
-// D0 -> RES
-// D2 -> DC
-// D8 -> CS
-// SSD1306Spi        display(D0, D2, D8);
-// or
-// SH1106Spi         display(D0, D2);
-
-// Initialize the OLED display using brzo_i2c
-// D3 -> SDA
-// D5 -> SCL
-// SSD1306Brzo display(0x3c, D3, D5);
-// or
-// SH1106Brzo  display(0x3c, D3, D5);
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
 
 // Initialize the OLED display using Wire library
-SSD1306  display(0x3c, 21, 22);
+SSD1306  display(0x3c, 21, 22); //i2c address for SSD1306 is 0x3c and is connected to pins D21 and D22
 
 #define uS_TO_m_FACTOR 60000000   // Conversion factor for micro seconds to minutes
 #define uS_TO_s_FACTOR 1000000   // Conversion factor for micro seconds to minutes
 #define TIME_TO_SLEEP 5        // Time ESP32 will go to sleep (in minutes)
-#define DEMO_DURATION 2000
+#define DISPLAY_ON_DURATION 2000
+
+const int SleepTime = TIME_TO_SLEEP * uS_TO_s_FACTOR;
 typedef void (*Demo)(void);
 
 int demoMode = 0;
@@ -78,21 +27,13 @@ void drawTimeDemo() {
     display.drawString(64, 10, "08:00");
 }
 
-void drawTimeDemo2() {
-    // Font Demo1
-    // create more fonts at http://oleddisplay.squix.ch/
-    display.setTextAlignment(TEXT_ALIGN_CENTER);
-    display.setFont(Lato_Light_38);
-    display.drawString(64, 10, "08:00");
-}
-
 void drawImageDemo() {
     // see http://blog.squix.org/2015/05/esp8266-nodemcu-how-to-create-xbm.html
     // on how to create xbm files
     display.drawXbm(34, 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
 }
 
-Demo demos[] = {drawTimeDemo, drawTimeDemo2};
+Demo demos[] = {drawTimeDemo};
 int demoLength = (sizeof(demos) / sizeof(Demo));
 long timeSinceLastModeSwitch = 0;
 
@@ -102,28 +43,18 @@ void setup() {
   Serial.println();
   Serial.println(ESP.getCpuFreqMHz());
   //We set our ESP32 to wake up every 1 minute
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_s_FACTOR);
+  esp_sleep_enable_timer_wakeup(SleepTime);
   
   display.init();
 
   display.flipScreenVertically();
 
   //display.clear();
-  display.setContrast(255);
+  display.setContrast(0);
   drawTimeDemo();
   display.display();
-  delay(DEMO_DURATION);
-  display.setContrast(0);
-  delay(DEMO_DURATION);
-  display.setContrast(255);
-  display.clear();
-  drawTimeDemo2();
-  display.display();
-  delay(DEMO_DURATION);
-  display.setContrast(0);
-  delay(DEMO_DURATION);
+  delay(DISPLAY_ON_DURATION);
   display.displayOff();
-  delay(DEMO_DURATION);
   esp_deep_sleep_start();
 }
 

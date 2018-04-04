d621b61d20b93cf33408c04548490726ab8b4ddb
Matt Parkinson <matt.g.parkinson@gmail.com>
2018-04-04 09:16:39 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,21 @@
 
 void setup()
 {
+  String batteryText; // String variable to hold text for battery voltage
+  float vBAT;         // Float variable to hold battery voltage
+
   Serial.begin(115200);         // Setup Serial output
   pinMode(INBUILT_LED, OUTPUT); // Set pin 5 as output - this pin also has the built in LED
   if (!firstStartComplete)      // If we booted from power on, not sleep...
   {
     startSequence(); // ...run the builtin led start sequence
   }
-  pinMode(ENCODER_BTN_SET_ALARM, INPUT_PULLUP); // Set pin 34 as input with pullup resistor
-  pinMode(ENCODER_CW_SET_ALARM, INPUT);         // Set pin 33 as input
-  pinMode(ENCODER_CCW_SET_ALARM, INPUT);        // Set pin 32 as input
-  encoder.setAccelerationEnabled(true);         // Enable acceleration on the rotary encoder
-
+  pinMode(ENCODER_BTN_SET_ALARM, INPUT_PULLUP);                                // Set pin 34 as input with pullup resistor
+  pinMode(ENCODER_CW_SET_ALARM, INPUT);                                        // Set pin 33 as input
+  pinMode(ENCODER_CCW_SET_ALARM, INPUT);                                       // Set pin 32 as input
+  encoder.setAccelerationEnabled(true);                                        // Enable acceleration on the rotary encoder
+  chargeServo.attach(CHARGE_SERVO_PIN,                                         // Attach servo on pin 23...
+                     CHARGE_SERVO_CHANNEL);                                    // ...using channel 1
   Serial.print("Clock speed: ");                                               // Debug
   Serial.println(ESP.getCpuFreqMHz());                                         // Debug
   Serial.println();                                                            // Debug
@@ -36,37 +40,46 @@ void setup()
   Serial.print("Encoder acceleration is ");                                    // Debug
   Serial.println((encoder.getAccelerationEnabled()) ? "enabled" : "disabled"); // Debug
 
-  Serial.println("Init Display");           // Debug
-  displayInit();                            // Initialise the OLED display
-  Serial.println("Init RTC");               // Debug
-  rtcInit();                                // Initialise the DS1307 RTC
-  chargeServo.attach(CHARGE_SERVO_PIN, 
-                     CHARGE_SERVO_CHANNEL);
-  Serial.println("Getting alarm settings"); // Debug
-  getAlarmSettings();                       // Retrieve alarm settings from preferences an store them
+  Serial.println("Init Display");                                         // Debug
+  displayInit();                                                          // Initialise the OLED display
+  Serial.println("Init RTC");                                             // Debug
+  rtcInit();                                                              // Initialise the DS1307 RTC
+  Serial.print("Servo is ");                                              // Debug
+  Serial.println((chargeServo.attached()) ? "attached" : "not attached"); // Debug
+  Serial.println("Getting alarm settings");                               // Debug
+  getAlarmSettings();                                                     // Retrieve alarm settings from preferences an store them
 
-  String batteryText;
-  float vBAT;
-  vBAT = ceilf(getBatteryVoltage() * 100) / 100;
-  batteryText = String(vBAT) + "V";
-  Serial.print("Battery Voltage: ");    // Debug (Prints battery voltage on Serial, this will be displayed somehow)
-  Serial.println(batteryText);          // Debug
-  
+  vBAT = ceilf(getBatteryVoltage() * 100) / 100; // Work out battery voltage from DAC and round to 2 decimal places
+  batteryText = String(vBAT) + "V";              // Populate the BatteryText variable
+  Serial.print("Battery Voltage: ");             // Debug (Prints battery voltage on Serial, this will be displayed somehow)
+  Serial.println(batteryText);                   // Debug
+
   switch (get_wakeup_reason())
   {        // Get wakeup reason
   case 3:  // We were woken by timer...
     break; // Nothing to do since our default action is to handle being woken by timer.
   case 4:  // We were woken by touchpad...
-  
-    //oled.setTextAlignment(TEXT_ALIGN_CENTER); // Align text to centre of the OLED display
-    //oled.setFont(DejaVu_Sans_40);             // Set the font for the text
-    //oled.drawString(64, 10, batteryText);     // Draw the time on the display
-    //oled.display();
-    //delay(1000);
-    showTime(); // ...so show the time on the OLED display
-    break;      // Exit Switch
-  default:      // We weren't woken by timer or touch...
-    break;      // Do Nothing
+
+    oled.setTextAlignment(TEXT_ALIGN_CENTER); // Align text to centre of the OLED display
+    oled.setFont(DejaVu_Sans_40);             // Set the font for the text
+    oled.drawString(64, 10, batteryText);     // Draw the time on the display
+    oled.display();                           // Display Voltage
+    delay(1000);                              // Keep on screen for 1 sec
+    showTime();                               // ...so show the time on the OLED display
+    break;                                    // Exit Switch
+  default:                                    // We weren't woken by timer or touch...
+    break;                                    // Do Nothing
+  }
+
+  if (vBAT <= 4.00F)
+  {                                                   // If vBAT is less than or equal to 4.00V...
+    Serial.println("DEBUG: Servo set to 90 degrees"); // Debug
+    chargeServo.write(90);                            // ...raise charge flag
+  }
+  else
+  {                                                  // ...otherwise...
+    Serial.println("DEBUG: Servo set to 0 degrees"); // Debug
+    chargeServo.write(0);                            // ...lower the charge flag
   }
 
   unsigned long starttime, endtime; // Declare the variables required for the 5 second loop
@@ -74,18 +87,7 @@ void setup()
   endtime = starttime;              // Set end time to starttime
 
   while ((endtime - starttime) <= DISPLAY_ON_DURATION)
-  {                    // do this loop for up to 5000mS (or value of "DISPLAY_ON_DURATION")
-      if (vBAT <= 4.00F)
-    { // If vBAT is less than or equal to 4.00V...
-      Serial.println("DEBUG: Servo set to 90 degrees");
-      chargeServo.write(90); // ...raise charge flag
-    }
-    else
-    { // ...otherwise...
-      Serial.println("DEBUG: Servo set to 0 degrees");
-      chargeServo.write(0);  // ...lower the charge flag
-    }
-    
+  { // do this loop for value of "DISPLAY_ON_DURATION"
     encoder.service(); // Poll the rotary encoder
     showTime();        // Display the time on the OLED display
 
